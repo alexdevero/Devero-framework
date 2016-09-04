0d9851b1d861dbb4c1c8d6a04500ec9c4f714b33
Alex Devero <deveroalex@gmail.com>
2016-09-04 08:20:11 +0200

    Added default app setting.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 // Your JavaScript goes here
+// @author Alex Devero <deveroalex@gmail.com>
+//
+
+(function () {
+  'use strict';
+
+  var app = {
+    appSettings: {
+      // Variables for switches
+    },
+    appControllers: {
+      // App controllers
+    },
+    appSwitches: function() {
+      // Conditions for switches
+      if () {}
+    },
+    init: function() {
+      // Remove 'no-js' class as a test of available JavaScript
+      if (document.querySelectorAll('.no-js').length > 0) {
+        document.querySelectorAll('.no-js')[0].classList.remove('no-js');
+      }
+
+      // Assign appControllers to 'window' object
+      window.appController = app.appControllers;
+
+      // Initiate appSwitches function
+      app.appSwitches();
+    }
+  };
+
+  // Initiate 'init' funcion
+  app.init();
+})();
